40aab445863a4e23176d96897a3ae961d833956c
iPhonig <bhonig@iphonig.com>
2015-04-19 17:06:16 -0400

    Initial Commit

---
+++ WWDC2015/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WWDC2015
+//
+//  Created by Ben Honig on 4/19/15.
+//  Copyright (c) 2015 iPhonig, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
